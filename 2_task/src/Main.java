fd7a9f8a99618db9e6628a0c65eabf1fcf1c84a3
irendoro <irinadorofeeva3958@gmail.com>
2025-08-31 17:21:09 +0300

    add second task

---
+++ 2_task/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double doubleVariable = 123.456789;
+        byte byteVariable = 127;
+
+        System.out.println("Double variable: " + doubleVariable);
+        System.out.println("Byte variable: " + byteVariable);
+    }
+}
